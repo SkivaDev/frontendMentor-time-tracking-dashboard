71294fc0362f03a2cae4e242dcc2f413e8a0ef0a
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-03-16 13:17:21 -0500

    logic was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import data from "./data.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentMode, setCurrentMode] = useState("daily");
+  const [amountOfTime, setAmountOfTime] = useState("Day");
+
+  const handleDailyBtn = () => {
+    setCurrentMode("daily");
+    setAmountOfTime('Day');
+  };
+  const handleWeeklyBtn = () => {
+    setCurrentMode("weekly");
+    setAmountOfTime('Week');
+
+  };
+  const handleMonthlyBtn = () => {
+    setCurrentMode("monthly");
+    setAmountOfTime('Month');
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <section className="">
+      <section className="">
+        <div className="">
+          <img src="./assets/images/image-jeremy.png" alt="image-jeremy" />
+          <h3 className="">Report for</h3>
+          <h1 className="">Jeremy Robson</h1>
+        </div>
+        <div className="">
+          <button className={``} onClick={handleDailyBtn}>
+            Daily
+          </button>
+          <button className={``} onClick={handleWeeklyBtn}>
+            Weekly
+          </button>
+          <button className={``} onClick={handleMonthlyBtn}>
+            Monthly
+          </button>
+        </div>
+      </section>
+      {data.map((item, i) => (
+        <section className="">
+          <div className="">
+            <img src={item.background} alt="" className="" />
+          </div>
+          <div className="">
+            <header className="">
+              <h3 className="">{item.title}</h3>
+              <div className="">***</div>
+            </header>
+            <div className="">
+              <h2 className="">
+                {item.timeframes[currentMode].current}hrs
+              </h2>
+              <p className="">
+                Last {amountOfTime} - {item.timeframes[currentMode].previous}hrs
+              </p>
+            </div>
+          </div>
+        </section>
+      ))}
+    </section>
+  );
 }
 
-export default App
+export default App;
