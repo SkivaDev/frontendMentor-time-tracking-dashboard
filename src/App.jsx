0f6e7b2218d1d0c4172089273aa494c0ebfe1eac
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-03-17 12:57:12 -0500

    first css part

--- src/App.jsx
+++ src/App.jsx
@@ -10,60 +10,65 @@ function App() {
 
   const handleDailyBtn = () => {
     setCurrentMode("daily");
-    setAmountOfTime('Day');
+    setAmountOfTime("Day");
   };
   const handleWeeklyBtn = () => {
     setCurrentMode("weekly");
-    setAmountOfTime('Week');
-
+    setAmountOfTime("Week");
   };
   const handleMonthlyBtn = () => {
     setCurrentMode("monthly");
-    setAmountOfTime('Month');
+    setAmountOfTime("Month");
   };
 
   return (
-    <section className="">
-      <section className="">
-        <div className="">
-          <img src="./assets/images/image-jeremy.png" alt="image-jeremy" />
-          <h3 className="">Report for</h3>
-          <h1 className="">Jeremy Robson</h1>
-        </div>
-        <div className="">
-          <button className={``} onClick={handleDailyBtn}>
-            Daily
-          </button>
-          <button className={``} onClick={handleWeeklyBtn}>
-            Weekly
-          </button>
-          <button className={``} onClick={handleMonthlyBtn}>
-            Monthly
-          </button>
-        </div>
-      </section>
-      {data.map((item, i) => (
-        <section className="">
-          <div className="">
-            <img src={item.background} alt="" className="" />
+    <div className="sm:flex sm:items-center sm:justify-center h-screen">
+      <section className="grid container gap-10 w-full h-auto">
+        <section className="profile bg-darkBlue">
+          <div className="profile-container ">
+            <img 
+              className="h-20 w-20 border-white border-4 rounded-full"
+              src="./assets/images/image-jeremy.png" 
+              alt="image-jeremy" 
+            
+            />
+            <h3 className="">Report for</h3>
+            <h1 className="">Jeremy Robson</h1>
           </div>
           <div className="">
-            <header className="">
-              <h3 className="">{item.title}</h3>
-              <div className="">***</div>
-            </header>
-            <div className="">
-              <h2 className="">
-                {item.timeframes[currentMode].current}hrs
-              </h2>
-              <p className="">
-                Last {amountOfTime} - {item.timeframes[currentMode].previous}hrs
-              </p>
-            </div>
+            <button className={``} onClick={handleDailyBtn}>
+              Daily
+            </button>
+            <button className={``} onClick={handleWeeklyBtn}>
+              Weekly
+            </button>
+            <button className={``} onClick={handleMonthlyBtn}>
+              Monthly
+            </button>
           </div>
         </section>
-      ))}
-    </section>
+        {data.map((item, i) => (
+          <section className={`w-full`}>
+            <div className="">
+              <img src={item.background} alt="" className="" />
+            </div>
+            <div className="bg-darkBlue px-6 py-3">
+              <header className="flex justify-between">
+                <h3 className="">{item.title}</h3>
+                <div className="">***</div>
+              </header>
+              <div className="">
+                <h2 className="">{item.timeframes[currentMode].current}hrs</h2>
+                <p className="">
+                  Last {amountOfTime} - {item.timeframes[currentMode].previous}
+                  hrs
+                </p>
+              </div>
+            </div>
+          </section>
+        ))}
+      </section>
+    </div>
   );
 }
 
